f0e6525670840c77c5eaaeac4870edc763d7b29a
Uriel Rolando Palomares Elias <uriel.palomares@kavak.com>
2023-08-22 15:37:14 -0600

    feat: add folder demo

---
+++ src/app/demo/service/customer.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CustomerService {
+
+    constructor(private http: HttpClient) { }
+
+    getCustomersSmall() {
+        return this.http.get<any>('assets/demo/data/customers-small.json')
+            .toPromise()
+            .then(res => res.data)
+            .then(data => data);
+    }
+
+    getCustomersMedium() {
+        return this.http.get<any>('assets/demo/data/customers-medium.json')
+            .toPromise()
+            .then(res => res.data)
+            .then(data => data);
+    }
+
+    getCustomersLarge() {
+        return this.http.get<any>('assets/demo/data/customers-large.json')
+            .toPromise()
+            .then(res => res.data)
+            .then(data => data);
+    }
+}
