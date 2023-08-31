39ad0310246280625b3a8b6195d7888f084f4ef9
Andres2D <andres2d1997.1206@gmail.com>
2023-08-31 17:38:53 -0500

    Add map instance to incident details

--- src/app/components/card-incident-details/card-incident-details.component.ts
+++ src/app/components/card-incident-details/card-incident-details.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import mapboxgl from 'mapbox-gl';
+import { AfterViewInit, Component, EventEmitter, Input, Output } from '@angular/core';
+import mapboxgl, { LngLatLike } from 'mapbox-gl';
 import { mockMapConfiguration } from '../maps/maps.mock';
 
 @Component({
@@ -7,29 +7,29 @@ import { mockMapConfiguration } from '../maps/maps.mock';
   templateUrl: './card-incident-details.component.html',
   styleUrls: ['./card-incident-details.component.scss'],
 })
-export class CardIncidentDetailsComponent {
+export class CardIncidentDetailsComponent implements AfterViewInit {
   @Input() incident: any;
   @Input() visible = false;
   @Output() closeEvent = new EventEmitter<null>();
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
     const {
       style,
       zoom
     } = mockMapConfiguration;
   
     mapboxgl.accessToken = import.meta.env.NG_APP_KEY_MAP;
-    const position = [parseFloat(this.incident.longitude), parseFloat(this.incident.latitude)]
+    const position: LngLatLike = [Number(this.incident.longitude), Number(this.incident.latitude)];
     const map = new mapboxgl.Map({
       container: 'searchMapDetails',
       style,
-      center: position as any,
+      center: position,
       zoom: zoom,
     });
-    new mapboxgl.Marker().setLngLat((position as any).center).addTo(map);
+    new mapboxgl.Marker().setLngLat(position).addTo(map);
   }
 
   getSeverity(status: any) {
     return 'danger';
-}
+  }
 }
